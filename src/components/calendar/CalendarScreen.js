19d2d528925263fc8bdf3dccaba428a9b303d5ce
Apoyo-Web <mpienso@gmail.com>
2021-10-10 20:14:10 +0200

    Big Calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+
+
 import { Navbar } from '../ui/Navbar'
+
+
+const localizer = momentLocalizer(moment)
+
+const events = [{
+    title: 'Cumpleaños de Beatriz',
+    start: moment().toDate(),
+    end: moment().add(2, 'hours').toDate(),
+    bgcolor: '#fafafa'
+    
+}]
 
 export const CalendarScreen = () => {
     return (
         <div>
             <Navbar />
-            <h1>Calendar Screen</h1>
+            <Calendar
+      localizer={localizer}
+      events={events}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
         </div>
     )
 }
