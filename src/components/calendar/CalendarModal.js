621642e6fe58d5c970528154e93dcd81359ae96a
Apoyo-Web <mpienso@gmail.com>
2021-10-13 17:35:24 +0200

    datePicker

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -1,22 +1,36 @@
 import React, { useState } from 'react'
 import Modal from 'react-modal';
+import DateTimePicker from 'react-datetime-picker';
 import { customStyles } from '../../helpers/customStyleModal';
 import './modal.css'
+import moment from 'moment';
 
 
 
 Modal.setAppElement('#root');
 export const CalendarModal = () => {
 
+    const now = moment().minute(0).seconds(0).add(1, 'hours')
+    const end = now.clone().add(1,'hours')
+  
     const [isOpen, setIsOpen] = useState(true)
+    const [dateStart, setDateStart] = useState(now.toDate());
+    const [dateEnd, setDateEnd] = useState(end.toDate());
 
     const closeModal = () => {
         setIsOpen(false)
     }
+
+    const handleStartDateChange = (e) => {
+        setDateStart(e)
+    }
+    const handleEndDateChange = (e) => {
+        setDateEnd(e)
+    }
     return (
         <Modal
-        isOpen={isOpen}
-        // onAfterOpen={afterOpenModal}
+        isOpen={true}
+        
         onRequestClose={closeModal}
             style={customStyles}
             closeTimeoutMS={200}
@@ -24,9 +38,62 @@ export const CalendarModal = () => {
             overlayClassName="modal-fondo"
         >
 
-            <h1>Hola mundo</h1>
-            <hr />
-            <span>Hola de nuevo...</span>
+<h1> Nuevo evento </h1>
+<hr />
+<form className="container">
+
+    <div className="form-group">
+        <label>Fecha y hora inicio</label>
+        <DateTimePicker
+        onChange={handleStartDateChange}
+        value={dateStart}
+        className="form-control"
+      />
+    </div>
+
+    <div className="form-group">
+        <label>Fecha y hora fin</label>
+        <DateTimePicker
+        onChange={handleEndDateChange}
+                        value={dateEnd}
+                        minDate={dateStart}
+        className="form-control"
+      />
+    </div>
+
+    <hr />
+    <div className="form-group">
+        <label>Titulo y notas</label>
+        <input 
+            type="text" 
+            className="form-control"
+            placeholder="Título del evento"
+            name="title"
+            autoComplete="off"
+        />
+        <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+    </div>
+
+    <div className="form-group">
+        <textarea 
+            type="text" 
+            className="form-control"
+            placeholder="Notas"
+            rows="5"
+            name="notes"
+        ></textarea>
+        <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+    </div>
+
+    <button
+        type="submit"
+        className="btn btn-outline-primary btn-block"
+    >
+        <i className="far fa-save"></i>
+        <span> Guardar</span>
+    </button>
+
+</form>
             
         </Modal>
     )
